aecec0a285604b8de507238fb0b75b2fa2cc04e6
Hercules Miranda <herculesmirandakaue@outlook.com>
2021-09-30 11:46:19 -0400

    manipulação básica de variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,13 @@
+var nome = 'hercules',
+    idade = 22,
+    comidafavorita;
+
+comidafavorita = 'xuxu';
+
+var carro,
+    moto,
+    barco,
+    bicicleta,
+    avião;
+    
+console.log(bicicleta);
