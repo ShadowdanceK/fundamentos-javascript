6a8c456ff2b7ebf0b60551683a1c5eae56b3a914
Hercules Miranda <herculesmirandakaue@outlook.com>
2021-10-25 12:26:04 -0400

    Iniciando o uso de operadores de atribuição e ternário.

---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,46 @@
+            //? Operadores de atribuição
+
+//* Podem funcionar como formas abreviadas
+
+var x = 5
+var y = 10
+x += y //* x = x + y (15)
+x -= y //* x = x - y (-5)
+x *= y //* x = x * y (50)
+x /= y //* x = x / y (0.5)
+x %= y //* x = x % y (0)
+x **= y //* x = x ** y (9765625)
+
+/* -------------------------------------------------------- */
+
+            //? Operador ternário
+
+//* Abreviação de condicionais com if e else 
+
+var idade = 19
+var podeBeber = (idade>= 18) ? 'Pode beber' : 'Não pode beber'; 
+console.log(podeBeber) //Pode beber
+
+//! condição ? true 
+//! condição : false 
+
+//* Geralmente utilizado quando precisamos atribuir um valor para uma variável, dependendo de uma condição
+
+/* -------------------------------------------------------- */
+
+            //? IF abreviado
+
+//* Não é necessário abrir e fechar as chaves {} quando retornamos apenas uma linha de código
+
+var possuiFaculdade = true
+if(possuiFaculdade) console.log('Possui faculdade')
+else console.log('Não possui faculdade')
+
+// ou
+
+if(possuiFaculdade)
+  console.log('Possui faculdade')
+else
+  console.log('Não possui faculdade')
+
+//* Neste caso a segunda opção nos fornece um código mais limpo facilitando sua leitura
