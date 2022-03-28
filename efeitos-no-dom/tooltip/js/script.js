1058f4b0ea44d016d64fa6735fc1f02f15ca5667
Hercules Miranda <herculesmirandakaue@outlook.com>
2022-03-28 17:43:12 -0400

    usando um objeto como callback para criar caixas de diálogo/menus de contexto

---
+++ efeitos-no-dom/tooltip/js/script.js
@@ -0,0 +1,17 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+initScrollSuave();
+
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+initAnimacaoScroll();
+
+import initAccordion from "./modules/accordion.js";
+initAccordion();
+
+import initTabNav from "./modules/tabnav.js";
+initTabNav();
+
+import initModal from "./modules/modal.js";
+initModal();
+
+import initTooltip from "./modules/tooltipe.js";
+initTooltip();
