fb80f3d3d90f883c16fc620ed947a464b7f7fb57
Hercules Miranda <herculesmirandakaue@outlook.com>
2022-03-28 14:03:22 -0400

    como criar um modal para telas de login, adsense e mensagens do sistema

---
+++ efeitos-no-dom/modal/js/script.js
@@ -0,0 +1,14 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+initScrollSuave();
+
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+initAnimacaoScroll();
+
+import initAccordion from "./modules/accordion.js";
+initAccordion();
+
+import initTabNav from "./modules/tabnav.js";
+initTabNav();
+
+import initModal from "./modules/modal.js";
+initModal();
